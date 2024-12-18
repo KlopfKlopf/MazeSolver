321cb40bb4dad9ac0a905d306291816de15481e9
KlopfKlopf9 <linus93@live.de>
2024-12-18 19:35:39 +0100

    Added functionalitye to draw lines on screen

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from window import Window
-
+from window import Window, Line, Point
 win = Window(800, 600)
+
+line1 = Line(Point(0, 0), Point(800, 600))
+line2 = Line(Point(500, 100), Point(100, 500))
+win.draw_line(line1, "red")
+win.draw_line(line2, "blue")
 win.wait_for_close()
--- window.py
+++ window.py
@@ -6,8 +6,8 @@ class Window:
         self.__width = width
         self.__height = height
         self.__root = Tk()
-        self.__root.title = "MazeSolver"
-        self.__canvas = Canvas()
+        self.__root.title("MazeSolver")
+        self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
         self.__canvas.pack()
         self.__isrunning = False
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
@@ -23,3 +23,21 @@ class Window:
     
     def close(self):
         self.__isrunning = False
+
+    def draw_line(self, line, fill_color="black"):
+        line.draw(self.__canvas, fill_color)
+
+class Point:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Line:
+
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+    
+    def draw(self, canvas, fill_color="black"):
+            canvas.create_line(self.p1.x,  self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width = 2)
