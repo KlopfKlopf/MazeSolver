eda8e6b7f87c204853ee897c4b286cffbb2c9577
KlopfKlopf9 <linus93@live.de>
2024-12-19 20:46:00 +0100

    Draw a line from Center of Cell to Center of another Cell

--- cell.py
+++ cell.py
@@ -7,22 +7,37 @@ class Cell:
         self.has_right_wall = has_right_wall
         self.has_top_wall = has_top_wall
         self.has_bottom_wall = has_bottom_wall
-        self.__x1 = x1
-        self.__x2 = x2
-        self.__y1 = y1
-        self.__y2 = y2
-        self.__win = win
+        self._x1 = x1
+        self._x2 = x2
+        self._y1 = y1
+        self._y2 = y2
+        self._win = win
 
     def draw(self):
         if self.has_left_wall:
-          line = Line(Point(self.__x1, self.__y1), Point(self.__x1, self.__y2))
-          self.__win.draw_line(line)
+          line = Line(Point(self._x1, self._y1), Point(self._x1, self._y2))
+          self._win.draw_line(line)
         if self.has_right_wall:
-          line = Line(Point(self.__x2, self.__y1), Point(self.__x2, self.__y2))
-          self.__win.draw_line(line)
+          line = Line(Point(self._x2, self._y1), Point(self._x2, self._y2))
+          self._win.draw_line(line)
         if self.has_top_wall:
-          line = Line(Point(self.__x1, self.__y1), Point(self.__x2, self.__y1))
-          self.__win.draw_line(line)
+          line = Line(Point(self._x1, self._y1), Point(self._x2, self._y1))
+          self._win.draw_line(line)
         if self.has_bottom_wall:
-          line = Line(Point(self.__x1, self.__y2), Point(self.__x2, self.__y2))
-          self.__win.draw_line(line)
+          line = Line(Point(self._x1, self._y2), Point(self._x2, self._y2))
+          self._win.draw_line(line)
+    
+    def draw_move(self, to_cell, undo=False):
+      center_x = self._x1 + ((self._x2 - self._x1) / 2)
+      center_y = self._y1 + ((self._y2 - self._y1) / 2)
+
+      to_center_x = to_cell._x1 + ((to_cell._x2 - to_cell._x1) / 2)
+      to_center_y = to_cell._y1 + ((to_cell._y2 - to_cell._y1) / 2)
+
+      line = Line(Point(center_x, center_y), Point(to_center_x, to_center_y))
+      fill_color = "red"
+      
+      if undo:
+        fill_color = "grey"
+      
+      self._win.draw_line(line, fill_color)
--- main.py
+++ main.py
@@ -6,9 +6,11 @@ win = Window(800, 600)
 # cell = Cell(100, 300, 500, 600, win, has_right_wall=False)
 # cell.draw()
 
-# cell = Cell(150, 200, 225, 300, win, has_top_wall=False)
-# cell.draw()
+cell1 = Cell(150, 200, 200, 250, win)
+cell1.draw()
 
-cell = Cell(250, 200, 350, 300, win, has_left_wall= False)
-cell.draw()
+cell2 = Cell(250, 200, 300, 250, win)
+cell2.draw()
+
+cell1.draw_move(cell2)
 win.wait_for_close()
